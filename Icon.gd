264ba2107bc69a91e1fc023b0d9d685a232c7ab0
Rafal Mi <rafal.m4@gmail.com>
2025-08-17 16:17:44 +0200

    Lesson #5: Coding 101 (Part 2)

---
+++ Icon.gd
@@ -0,0 +1,19 @@
+extends Sprite3D
+
+var coins = 5
+var player_name = "robot"
+const SPEED = 2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("ui_left"):
+		rotate_y(deg_to_rad(-SPEED))
+	elif Input.is_action_pressed("ui_right"):
+		rotate_y(deg_to_rad(SPEED))
+	
+	pass
