127fd3937b39b00f2b194ae93f4bdd05a2d36df4
Rafal Mi <rafal.m4@gmail.com>
2025-08-23 13:24:17 +0200

    Lesson #12: Camera Controls & Camera-Relative Player Movement!

--- steve.gd
+++ steve.gd
@@ -1,14 +1,19 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 12
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-
 func _physics_process(delta):
+	
+	# Rotate the camera left / right
+	if Input.is_action_just_pressed("cam_left"):
+		$Camera_Controller.rotate_y(deg_to_rad(-30))
+	if Input.is_action_just_pressed("cam_right"):
+		$Camera_Controller.rotate_y(deg_to_rad(30))
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -18,9 +23,15 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	# New Vector3 direction, taking into account the user arrow inputs and the camera rotation
+	var direction = ($Camera_Controller.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	# Rotate the character mesh so oriented towards the direction moving in relation to camera
+	if input_dir != Vector2(0, 0):
+		$MeshInstance3D.rotation_degrees.y = $Camera_Controller.rotation_degrees.y - rad_to_deg(input_dir.angle()) + 270
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
